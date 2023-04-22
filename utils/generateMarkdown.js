258e7a1a80d6ca5b7d149df87eec29e0ded91069
cbae <xchris.bae@gmail.com>
2023-04-22 11:19:13 -0700

    testing license rendering section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,19 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return ( 
+      `## License
+       
+        Copyright @ ${license}. All rights reserved.
+
+        Licensed under the ${license} license.`
+    )
+  } else {
+    return ``;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -52,9 +64,8 @@ function generateMarkdown(data) {
 
   ${data.usage}
 
-  ## License
+  ${renderLicenseSection(data.license)}
   ${renderLicenseBadge(data.license)}
-  ${data.license}
 
   ## Credits
   
