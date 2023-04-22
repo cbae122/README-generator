7e19dfecb74462e7f242cec91daa22c15d3717d6
cbae <xchris.bae@gmail.com>
2023-04-22 10:17:52 -0700

    testing writeFile

--- index.js
+++ index.js
@@ -15,8 +15,10 @@
 // THEN I am taken to the corresponding section of the README
 
 // TODO: Include packages needed for this application
+const fs = require('fs')
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown')
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -73,7 +75,18 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(answers => {
+            fs.writeFile('dist/README.md', generateMarkdown(answers), err => {
+                if (err) return console.error(err);
+
+                console.log('README generated successfully...');
+            });
+            console.log(generateMarkdown(answers));
+        });
+}
 
 // Function call to initialize app
 init();
